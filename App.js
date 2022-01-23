84f4c706e6065466e725cff0fda6f24fc32acf1e
ssu00 <rachel4535@naver.com>
2022-01-23 19:58:42 +0900

    ⚡️ added editNotice and requiredDocument screen

--- App.js
+++ App.js
@@ -1,27 +1,35 @@
 import React from 'react';
-import {StyleSheet, StatusBar} from 'react-native';
+import {StyleSheet, StatusBar, View} from 'react-native';
 import MapView from './src/MapView';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
+import EditNotice from './src/screens/service/editNotice';
+import RequireDocument from './src/screens/service/requiredDocument';
 
 const App = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
+    <View style={styles.block}>
+      {/* <StatusBar barStyle="dark-content" />
       <MapView
         appKey="l7xx9d4d587fe7104a57b8feda886c846d1f"
         style={styles.map}
         lat={48.577741}
         lng={27.602706}
-      />
-    </>
+      /> */}
+      {/* <EditNotice /> */}
+      <RequireDocument />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  map: {
+  // map: {
+  //   flex: 1,
+  //   backgroundColor: '#fff',
+  // },
+  block: {
     flex: 1,
-    backgroundColor: Colors.lighter,
+    backgroundColor: '#fff',
   },
 });
 
